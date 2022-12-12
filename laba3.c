f50cdb2e4d9a0a06feb50686203a9c5739fe9fc0
Andrey2246 <91645174+Andrey2246@users.noreply.github.com>
2022-12-12 13:49:00 +0700

    ttt

--- laba3.c
+++ laba3.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(){
+    char st[255];
+    char prev = ' ';
+    char now;
+    int i = 0;
+    st[0] = ' ';
+    scanf(" %c", &now);
+    while (now != '\0'){
+        if (now == ' '){
+            continue;
+        }
+        if(prev == ' '){
+            prev = now;
+            continue;
+        }
+        if (now == st[i]){
+            i--;
+            prev = ' ';
+        }
+        else{
+            st[i] = prev;
+            prev = now;
+            i++;
+        }
+        scanf("%c", &now);
+    }
+    for (int j = 0;j < i;j++){
+        printf("%c", st[j]);
+    }
+    return 0;
+}
